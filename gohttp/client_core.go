b41ab2057de1c697ff309eba88a8b89e87c1d6eb
7045kHz <7mhz.cw@gmail.com>
2021-07-28 17:07:13 -0400

    test4

--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -6,11 +6,18 @@ import (
 )
 
 func (c *httpClient) do(m string, u string, h http.Header, b interface{}) (*http.Response, error) {
+
 	client := http.Client{}
 
 	request, err := http.NewRequest(m, u, nil)
 	if err != nil {
 		return nil, errors.New("Unable to create new request")
+	}
+	// Adding custom headers
+	for header, value := range h {
+		if len(value) > 0 {
+			request.Header.Set(header, value[0])
+		}
 	}
 	return client.Do(request)
 
