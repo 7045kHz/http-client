d92c529cd094ba64f9f8b63c5de11dea958bc7a3
7045kHz <7mhz.cw@gmail.com>
2021-07-29 20:59:53 -0400

    Updated to include NTLMv2 support

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A production-ready HTTP client in Go with lots of useful features and using nothing more than the standard library of the language.
 
+## About Original Source Location
+> This version has been modified to include NTLMv2 support. The original source can be found here: 
+[github.com/federicoleon/go-httpclient](github.com/federicoleon/go-httpclient) This was from an excellent class on creating http clients.  [https://www.udemy.com/course/how-to-develop-a-productive-http-client-in-golang-go](https://www.udemy.com/course/how-to-develop-a-productive-http-client-in-golang-go) 
+
+
+Future planned changes are to allow for a json config file to populate the client for running tests against API's.
 ## Installation
 
 ```bash
---
+++ examples/example_connect_github.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/7045kHz/http-client/gohttp"
+	"github.com/7045kHz/http-client/gomime"
+)
+
+type Endpoints struct {
+	CurrentUserUrl    string `json:"current_user_url"`
+	AuthorizationsUrl string `json:"authorizations_url"`
+	RepositoryUrl     string `json:"repository_url"`
+}
+
+var (
+	httpClient = getHttpClient()
+)
+
+func main() {
+
+	GetEndpoints()
+	GetEndpoints()
+	GetEndpoints()
+	GetEndpoints()
+}
+func getHttpClient() gohttp.Client {
+	headers := make(http.Header)
+	headers.Set(gomime.HeaderContentType, gomime.ContentTypeJson)
+	fmt.Println("Starting new client")
+	client := gohttp.NewBuilder().
+		SetHeaders(headers).
+		SetConnectionTimeout(2 * time.Second).
+		SetResponseTimeout(3 * time.Second).
+		SetUserAgent("Fedes-Computer").
+		Build()
+	return client
+}
+func GetEndpoints() (*Endpoints, error) {
+	// Make the request and wait for the response:
+	response, err := httpClient.Get("https://api.github.com")
+	if err != nil {
+		// Deal with the error as you need:
+		return nil, err
+	}
+
+	// Interacting with the response:
+	fmt.Println(fmt.Sprintf("Status Code: %d", response.StatusCode))
+	fmt.Println(fmt.Sprintf("Status: %s", response.Status))
+	//fmt.Println(fmt.Sprintf("Body: %s\n", response.String()))
+
+	// Processing JSON responses:
+	var endpoints Endpoints
+	if err := response.UnmarshalJson(&endpoints); err != nil {
+		// Deal with the unmarshal error as you need:
+		return nil, err
+	}
+
+	fmt.Println(fmt.Sprintf("Repository URL: %s", endpoints.RepositoryUrl))
+	return &endpoints, nil
+}
