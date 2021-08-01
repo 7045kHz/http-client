6db0ec2ff778db8a0df6caa5aa961166c19a0326
7045kHz <7mhz.cw@gmail.com>
2021-07-31 20:02:53 -0400

    Example HTTP Test Server that responds with body and headers of client

---
+++ examples/benchmarking/server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Request at %v\n", time.Now())
+	fmt.Fprintf(w, "URL: %v\n", r.URL)
+	fmt.Fprintf(w, "Request at %v\n", time.Now())
+	fmt.Fprintf(w, "Method: %v\n", r.Method)
+	fmt.Fprintf(w, "Content Length: %v\n", r.ContentLength)
+	data, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "Body: %v\n", string(data))
+	fmt.Fprintf(w, "TLS: %v\n", r.TLS)
+	fmt.Fprintf(w, "Host: %v\n", r.Host)
+	fmt.Fprintf(w, "URL User: %v\n", r.URL.User)
+	fmt.Fprintf(w, "Remove Address: %v\n", r.RemoteAddr)
+	fmt.Fprintf(w, "Request URI: %v\n", r.RequestURI)
+	fmt.Fprintf(w, "Response: %v\n", r.Response)
+	for k, v := range r.Header {
+		fmt.Printf("%v: %v\n", k, v)
+		fmt.Fprintf(w, "%v:%v\n", k, v)
+	}
+}
+
+func main() {
+	fmt.Printf("Starting Server at %v\n", time.Now())
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9090", nil)
+}
